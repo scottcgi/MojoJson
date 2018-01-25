fe87186cca4ddeb3b212c3f24ff68e2fba4c8781
scott.cgi <scott.cgi@qq.com>
2018-01-25 19:59:10 +0800

    Code format

--- MojoUnityJson.cs
+++ MojoUnityJson.cs
@@ -468,40 +468,54 @@ namespace MojoUnity
             {
                 case '0':
                     return 0;
+
                 case '1':
                     return 1;
+
                 case '2':
                     return 2;
+
                 case '3':
                     return 3;
+
                 case '4':
                     return 4;
+
                 case '5':
                     return 5;
+
                 case '6':
                     return 6;
+
                 case '7':
                     return 7;
+
                 case '8':
                     return 8;
+
                 case '9':
                     return 9;
 
                 case 'A':
                 case 'a':
                     return 10;
+
                 case 'B':
                 case 'b':
                     return 11;
+
                 case 'C':
                 case 'c':
                     return 12;
+
                 case 'D':
                 case 'd':
                     return 13;
+
                 case 'E':
                 case 'e':
                     return 14;
+
                 case 'F':
                 case 'f':
                     return 15;
